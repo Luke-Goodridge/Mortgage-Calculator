89bc532eb1668d2b3d1e7c17736ff723f613f60c
DESKTOP-R53PPF3\luke_ <lukegoodridge@icloud.com>
2020-08-04 20:14:13 +0100

    Started the project using the create-react-app boiler plate

---
+++ src/app-container/app-container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const app_container = (props) => {
+    return (
+        <div className="App">
+            <h1>Mortgage Calculator</h1>
+            <button>Monthly payments calculator</button>
+            <button>Max mortgage calculator</button>
+        </div>
+    );
+}
+
+export default app_container;
